1bd29cda89ef18a5569e6455068ce9fc4399b82d
Eric Browning <eric@coffeeandcode.com>
2017-07-05 15:29:38 -0400

    add templating file and work with click events

--- prototype.js
+++ prototype.js
@@ -1,7 +1,21 @@
 (function($){
 	$(function() {
+    initCard();
+    initEvents();
 		initFocus();
 	});
+  function initCard() {
+    var cards = '';
+    for ( var i = 0; i < 16; i++ ) {
+      cards += tmpl('tmpl_card', {});
+    }
+    $('ul.cards').html(cards);
+  }
+  function initEvents() {
+    $('.arrange .large').on('click', function() {
+      console.log('wahoo!');
+    });
+  }
   function initFocus() {
     $('.search input').focus();
   }
