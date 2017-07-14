5d8e4b097fb67d1263bd14994e2c5e7c70df6a2f
Eric Browning <eric@coffeeandcode.com>
2017-07-14 16:03:56 -0400

    beginning random number section

--- prototype.js
+++ prototype.js
@@ -1,5 +1,8 @@
 (function($){
-	$(function() {
+  var avatarColor = [];
+	var cardData = [];
+  $(function() {
+    initCardDetails();
     initCard();
     initEvents();
 		initFocus();
@@ -7,16 +10,51 @@
   function initCard() {
     var cards = '';
     for ( var i = 0; i < 16; i++ ) {
-      cards += tmpl('tmpl_card', {});
+      var cardDetails = {
+        "avatarColor" : cardData[i].avatarColor,
+        "id" : i,
+        "imgColor" : cardData[i].imgColor,
+        "likes" : cardData[i].likes,
+        "username" : cardData[i].username,
+        "views" : cardData[i].views
+      }
+      cards += tmpl('tmpl_card', cardDetails);
     }
     $('ul.cards').html(cards);
   }
+  function initCardDetails() {
+    for ( var i = 0; i <  16; i++ ) {
+      cardData.push({
+        "avatarColor" : "#fcc",
+        "id" : i,
+        "imgColor" : "#663399",
+        "likes" : "312",
+        "username" : "DesignPro",
+        "views" : "1501"
+      });
+      
+    }
+  }
   function initEvents() {
-    $('.arrange .large').on('click', function() {
-      console.log('wahoo!');
+    var large = $('.arrange .large');
+    var main = $('section.main');
+    var small = $('.arrange .small');
+    large.on('click', function() {
+      main.toggleClass('big');
+      $(this).addClass('active');
+      small.removeClass('active');
+
     });
+    small.on('click', function() {
+      main.toggleClass('big');
+      large.removeClass('active');
+      $(this).addClass('active');
+    });
+    console.log($('section.main').hasClass('big'));
+    console.log($('.arrange .small').hasClass('active'));
   }
   function initFocus() {
     $('.search input').focus();
   }
+  //
 })(jQuery);
